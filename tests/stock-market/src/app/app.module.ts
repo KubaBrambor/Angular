2d8bde43f7610191f86318242d1b466cda6561da
Jakub Brambor <jbrambor@ad.corp.google.com>
2019-12-05 09:21:35 +0100

    angular exercises with form validators

--- tests/stock-market/src/app/app.module.ts
+++ tests/stock-market/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { StockItemComponent } from './stock/stock-item/stock-item.component';
+import { Ex150Component } from './ex150/ex150.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StockItemComponent
+    StockItemComponent,
+    Ex150Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
