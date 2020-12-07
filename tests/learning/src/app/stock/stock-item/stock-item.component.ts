926e176cb034f8bd82da3335b97e2d25440ea843
KubaBrambor <kuba.brambor@gmail.com>
2020-12-07 20:04:26 +0100

    practicing ngClass, ngStyle and other class bindigs

--- tests/learning/src/app/stock/stock-item/stock-item.component.ts
+++ tests/learning/src/app/stock/stock-item/stock-item.component.ts
@@ -10,21 +10,26 @@ export class StockItemComponent implements OnInit {
   public stock: Stock;
   public obligacje: Obligacje;
   public stocks: any[];
+  public stockStyles: {};
   public stockClasses: {};
   constructor() { }
 
   ngOnInit(): void {
-    this.stock = new Stock('Company One', 'CMO', 20, 24);
+    this.stock = new Stock('Company One', 'CMO', 20, 40);
     this.obligacje = new Obligacje('CD Projekt Red', 'CDP', 40, 1, 'Polska')
     this.stocks = [this.stock, this.obligacje];
     let diff = (this.stock.prize/this.stock.previousPrize) - 1;
     let largeChange = Math.abs(diff) > 0.4;
+    this.stockStyles = {
+      "color": this.stock.isPositiveChange() ? "green" : "red",
+      "font-size": largeChange ? "25px" : "10px"
+    };
     this.stockClasses = {
       "positive": this.stock.isPositiveChange(),
       "negative": !this.stock.isPositiveChange(),
       "large-change": largeChange,
       "small-change": !largeChange
-    };
+    }
   }
 
   addToFavourite(event) {
